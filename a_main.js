6990aea553cfcfd4c39f236bf35ea25456bfe2bc
singhtej <singhtej@hotmail.com>
2019-01-07 17:14:25 +0100

    hide the data by clicking the button

--- a_main.js
+++ a_main.js
@@ -1,13 +1,24 @@
 Vue.component('message', {
 
     props: ['title', 'body'],
+     
+    data(){
+        return{
+            isVisible:true
+        };
+    },
+    
+    
+    
+    
+    
     template: `
-        <article class ="message">
+        <article class ="message" v-show="isVisible">
         <div class= "message-header">
 
             {{ title}}
             
-           
+            <button type="button" @click= "isVisible= false">x</button>
         
         </div>
         <div class="message-body">
@@ -19,6 +30,7 @@ Vue.component('message', {
        </article>
         
     `
+   
 });
 
 
