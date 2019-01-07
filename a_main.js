33d0af5ede97015a3bd3c1b6d7bc8142fd83d774
singhtej <singhtej@hotmail.com>
2019-01-07 17:05:57 +0100

    creating a simple model

---
+++ a_main.js
@@ -0,0 +1,27 @@
+Vue.component('message', {
+
+    props: ['title', 'body'],
+    template: `
+        <article class ="message">
+        <div class= "message-header">
+
+            {{ title}}
+            
+           
+        
+        </div>
+        <div class="message-body">
+        
+            {{ body }}
+
+        </div>
+
+       </article>
+        
+    `
+});
+
+
+new Vue({
+    el: '#root'
+});
