181e73a439b09e1af88dbed78bc8e83a683ef047
singhtej <singhtej@hotmail.com>
2019-01-08 01:45:42 +0100

    click on button it<#s show then close and try again it's work

--- aa_main.js
+++ aa_main.js
@@ -1,39 +1,34 @@
-Vue.component('message', {
+Vue.component('modal', {
 
-    props: ['title', 'body'],
-     
-    data(){
-        return{
-            isVisible:true
-        };
-    },
-    
-    
-    
-    
-    
     template: `
-        <article class ="message" v-show="isVisible">
-        <div class= "message-header">
-
-            {{ title}}
-            
-            <button type="button" @click= "isVisible= false">x</button>
         
+
+        <div class="modal is-active ">
+             <div class="modal-background"></div>
+                 <div class="modal-content">
+                    <div class="box">
+                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>   
+    
+                </div>
+                <button class="modal-close" @click="$emit('close')"></button>
         </div>
-        <div class="message-body">
-        
-            {{ body }}
-
-        </div>
-
-       </article>
-        
+    
+    
     `
-   
+
+       
+        
+    
+  
+
 });
 
 
 new Vue({
-    el: '#root'
+    el: '#root',
+
+    data:{
+        showModal:false
+    }
 });
