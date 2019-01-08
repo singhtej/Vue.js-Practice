140dae05758fb281c2ec55cca7e9bf9a6f82ce2c
singhtej <singhtej@hotmail.com>
2019-01-08 01:19:48 +0100

    modal box

---
+++ aa_main.js
@@ -0,0 +1,39 @@
+Vue.component('message', {
+
+    props: ['title', 'body'],
+     
+    data(){
+        return{
+            isVisible:true
+        };
+    },
+    
+    
+    
+    
+    
+    template: `
+        <article class ="message" v-show="isVisible">
+        <div class= "message-header">
+
+            {{ title}}
+            
+            <button type="button" @click= "isVisible= false">x</button>
+        
+        </div>
+        <div class="message-body">
+        
+            {{ body }}
+
+        </div>
+
+       </article>
+        
+    `
+   
+});
+
+
+new Vue({
+    el: '#root'
+});
