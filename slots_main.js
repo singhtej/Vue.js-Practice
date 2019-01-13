46cef43c75b097787b0d600f83778f5b59964b13
singhtej <singhtej@hotmail.com>
2019-01-13 14:51:44 +0100

    individual slot for header and footer

--- slots_main.js
+++ slots_main.js
@@ -7,20 +7,25 @@ Vue.component('modal',{
     <div class="modal-card">
       <header class="modal-card-head">
         <p class="modal-card-title">
-        //creating a slot for header
-        <slot></slot>
+        
+
+        <slot name="header"></slot>
         </p>
+
         <button class="delete" aria-label="close"></button>
       </header>
 
       <section class="modal-card-body">
-        Temporary Content for now
+        <slot></slot>
       </section>
 
 
       <footer class="modal-card-foot">
-        <button class="button is-success">Save changes</button>
-        <button class="button">Cancel</button>
+       
+        <slot name="footer">
+            <button class="button is-success">Save changes</button>
+            <button class="button">Cancel</button>
+        </slot>
       </footer>
 
 
@@ -29,7 +34,7 @@ Vue.component('modal',{
     
     `
 })
-
+// creating a slot for each individual
 
 // So far, we've only reviewed default component slots. Or, in other words, "take this bit of content, and insert it right there, as designated by the slot tags. But what about when we require more control? A modal component is a perfect example of this. If you want to slot a header section in one place, and a footer section in another, named slots are exactly what you need.
 new Vue({
